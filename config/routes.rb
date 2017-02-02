e69cf8040af78ae2f551c66a7c54defdab93a3ec
Aaron Groch <ar.groch@gmail.com>
2017-02-02 12:11:19 -0500

    in-class work - making API calls and practicing creating custom forms

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'welcome/test'
+
+  root 'welcome#index'
+  # get '/' => 'welcome#index'
+
+  get 'index' => 'welcome#index'
+
+  post 'index' => 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
